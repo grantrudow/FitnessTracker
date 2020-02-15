8a973b21cdbe450cd41c41e8d13e7ce4b9ee1148
grantrudow <58617204+wrangler13@users.noreply.github.com>
2020-02-15 14:11:08 -0500

    Initial Commit

---
+++ FitnessTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FitnessTracker
+//
+//  Created by Grant Rudow on 2/15/20.
+//  Copyright © 2020 Grant Rudow. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
